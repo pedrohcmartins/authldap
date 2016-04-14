104508039754e60d36f644a2aecbb2ab970ec3c8
Dagnaldo <naldo.penha@gmail.com>
2016-04-14 17:40:04 -0300

    update utils.py

--- authldap/utils.py
+++ authldap/utils.py
@@ -6,11 +6,11 @@ def validateUsername(request):
     
     regex = re.compile('([^\\s]+)\\@[^\\s]+')
 
-    if settings.USERNAME_SUFIX is not None:
-        if (request.__getitem__('username').find(settings.USERNAME_SUFIX)) >= 0:
+    if settings.USERNAME_SUFFIX is not None:
+        if (request.__getitem__('username').find(settings.USERNAME_SUFFIX)) >= 0:
             matched = regex.match(request.__getitem__('username'))
             if matched:
-                name = "%s%s" % (matched.group(1), settings.USERNAME_NEW_SUFIX)
+                name = "%s%s" % (matched.group(1), settings.USERNAME_NEW_SUFFIX)
                 request.__setitem__('username', name)
 
     return request
