7625a4c4531b6fffbd8c94ed86688b6985968370
dagnaldo <naldo.penha@gmail.com>
2016-04-19 14:38:40 -0300

    Creating test for userlog model

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -14,8 +14,6 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-from rest_framework.test import force_authenticate
-from rest_framework.test import APIClient
 
 from authldap.models import UserLog
 
@@ -25,17 +23,19 @@ client = Client()
 class TestAuthldap(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user(username='user1',
-                                             password="secret1",
-                                             email="user1@user.com",
-                                             first_name="new_user"
-                                             )
-        self.user2 = User.objects.create_user(username='user2',
-                                              password="secret2",
-                                              email="user2@user.com",
-                                              first_name="old_user",
-                                              is_superuser=True
-                                              )
+        self.user = User.objects.create_user(
+            username='user1',
+            password="secret1",
+            email="user1@user.com",
+            first_name="new_user"
+        )
+        self.user2 = User.objects.create_user(
+            username='user2',
+            password="secret2",
+            email="user2@user.com",
+            first_name="old_user",
+            is_superuser=True
+        )
 
     def test_response_url(self):
         self.assertEqual(User.objects.count(), 2)
@@ -50,7 +50,10 @@ class TestAuthldap(TestCase):
         self.assertEqual(response.data.get('email'), 'user1@user.com')
         self.assertEqual(response.data.get('name'), 'new_user')
         self.assertEqual(response.data.get('token'), token)
-        self.assertEqual(response.data.get('suser'), False)
+        self.assertFalse(False)
+        self.assertEqual(UserLog.objects.filter(user=self.user).count(), 1)
+        self.assertEqual(UserLog.objects.get(user=self.user).type, 'login')
+        self.assertEqual(UserLog.objects.count(), 1)
 
         response = self.client.post(
             reverse('token-auth'),
@@ -63,7 +66,11 @@ class TestAuthldap(TestCase):
         self.assertEqual(response.data.get('email'), 'user2@user.com')
         self.assertEqual(response.data.get('name'), 'old_user')
         self.assertEqual(response.data.get('token'), token)
-        self.assertEqual(response.data.get('suser'), True)
+        self.assertTrue(response.data.get('suser'))
+        self.assertEqual(UserLog.objects.filter(user=self.user2).count(), 1)
+        self.assertEqual(UserLog.objects.get(user=self.user2).type, 'login')
+        self.assertEqual(UserLog.objects.filter(type='login').count(), 2)
+        self.assertEqual(UserLog.objects.count(), 2)
 
     def tearDown(self):
         self.user.delete()
