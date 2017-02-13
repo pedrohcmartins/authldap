f8cf36202859cf55eed495c827b26892e412ba40
dagnaldo <naldo.penha@gmail.com>
2017-02-13 19:27:54 -0200

    Adding change password url

--- authldap/urls.py
+++ authldap/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import AuthToken, LogoutView
+from .views import AuthToken, LogoutView, ChangePasswordView
 
 
 app_name = 'authldap'
@@ -8,6 +8,7 @@ app_name = 'authldap'
 urlpatterns = [
 
     url(r'^login/', AuthToken.as_view(), name='token-auth'),
-    url(r'^logout/', LogoutView.as_view(), name='logout')
+    url(r'^logout/', LogoutView.as_view(), name='logout'),
+    url(r'^change-pass/', ChangePasswordView.as_view(), name='change-pass'),
 
 ]
