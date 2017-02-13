18035cfa81ff8a1ca8c739a707c2c89d4aa71c15
dagnaldo <naldo.penha@gmail.com>
2017-02-13 19:28:40 -0200

    Changes on utils to validate setting data, and created tests to implement ldap validation

--- authldap/utils.py
+++ authldap/utils.py
@@ -3,10 +3,11 @@ from django.conf import settings
 
 
 def validate_username(request):
-    
+    """ Returns a validated username with USERNAME_SUFFIX from settings """
     regex = re.compile('([^\\s]+)\\@[^\\s]+')
 
-    if settings.USERNAME_SUFFIX is not None:
+    # try settings.USERNAME_SUFFIX:
+    if hasattr(settings, 'USERNAME_SUFFIX'):
         if (request.__getitem__('username').find(settings.USERNAME_SUFFIX)) >= 0:
             matched = regex.match(request.__getitem__('username'))
             if matched:
@@ -14,3 +15,10 @@ def validate_username(request):
                 request.__setitem__('username', name)
 
     return request
+
+def get_user_data(arr_list, data):
+    """Returns a object with attributes from 'data' param """
+    obj = {}
+    for k, value in arr_list.items():
+        obj[k] = data.__getattribute__(value)
+    return obj
--- tests/test_urls.py
+++ tests/test_urls.py
@@ -8,15 +8,16 @@ test_authldap
 Tests for `authldap` models module.
 """
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.test import TestCase, Client
+from django.test import TestCase
 from rest_framework.authtoken.models import Token
+from rest_framework.test import (
+    APIClient, APIRequestFactory, force_authenticate
+)
 
 from authldap.models import UserLog
-
-client = Client()
-
 
 class TestAuthldap(TestCase):
     def setUp(self):
@@ -89,5 +90,99 @@ class TestAuthldap(TestCase):
 
         self.assertEqual(response.data.get('id'), 3)
 
+
+    def test_response_with_user_model_response(self):
+        settings.AUTH_USER_RESPONSE = {
+            'username': 'username'
+        }
+
+        response = self.client.post(
+            reverse('token-auth'),
+            {'username': 'user1',
+             'password': 'secret1'}
+        )        
+
+        self.assertFalse(response.data.get('is_staff'))
+        self.assertFalse(response.data.get('is_superuser'))
+        self.assertEqual(response.data['username'], 'user1')
+
+    def test_ldap_response(self):
+
+        if hasattr(settings, 'LDAP_AUTH_URL')\
+            and hasattr(settings, 'LDAP_USER')\
+            and hasattr(settings, 'LDAP_PASSWORD'):
+            response = self.client.post(
+                reverse('token-auth'),
+                {'username': settings.LDAP_USER,
+                 'password': settings.LDAP_PASSWORD}
+            )        
+            self.assertEqual(response.status_code, 200)
+
     def tearDown(self):
         self.user.delete()
+
+
+class TestAuthenticationUrl(TestCase):
+    """docstring for TestAuthenticationUrl"""
+    def setUp(self):
+        self.url_change_pass = reverse('change-pass')
+        self.url_login = reverse('token-auth')
+        self.raw_pass = User.objects.make_random_password()
+        self.user = User.objects.create_user(
+            username='user1',
+            password=self.raw_pass,
+            email="user1@user.com",
+            first_name="new_user"
+        )
+
+    def test_auth_url(self):
+        
+        login = self.client.post(
+            reverse('token-auth'),
+            {'username': 'user1',
+             'password': self.raw_pass}
+        )
+
+        self.assertEqual(login.status_code, 200)
+        self.assertEqual(login.data['email'], 'user1@user.com')
+        self.assertEqual(login.data['email'], 'user1@user.com')
+        token = login.data['token']
+
+        client = APIClient()
+        client.force_authenticate(user=self.user, token=token)
+
+        request = client.put(
+            self.url_change_pass,
+            {
+                'new_pass': 'user1',
+                'old_pass': self.raw_pass
+            }
+        )
+
+        self.assertEqual(request.status_code, 200)
+        
+        login_error = self.client.post(
+            reverse('token-auth'),
+            {'username': 'user1',
+             'password': self.raw_pass}
+        )
+        self.assertEqual(login_error.status_code, 400)
+        self.assertEqual(login_error.data['non_field_errors'],
+            ['Unable to log in with provided credentials.'] )
+
+        login = self.client.post(
+            reverse('token-auth'),
+            {'username': 'user1',
+             'password': 'user1'}
+        )
+        self.assertEqual(login.status_code, 200)
+
+    def tearDown(self):
+        for token in Token.objects.all():
+            token.delete()
+
+        for user in User.objects.all():
+            user.delete()
+
+        self.assertEqual(Token.objects.count(), 0)
+        self.assertEqual(User.objects.count(), 0)
